1b6aa3047b968808e92f7c2c1312a67f02116382
Tim Clements <thclements@g.harvard.edu>
2020-06-22 19:20:29 -0400

    Add function to load test data

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 using Dates
 ############################
-# Install P + S models
+# Install P + S models + sample data
 ############################
 base = dirname(@__FILE__)
 download_dir = joinpath(base, "downloads")
@@ -8,16 +8,22 @@ download_dir = joinpath(base, "downloads")
 mkpath(download_dir)
 pmodel = joinpath(download_dir,"model_P.pt")
 smodel = joinpath(download_dir,"model_S.pt")
+edata = joinpath(download_dir,"2016.162.00.00.00.008.AZ.TRO.HHE.SAC")
+ndata = joinpath(download_dir,"2016.162.00.00.00.008.AZ.TRO.HHN.SAC")
+zdata = joinpath(download_dir,"2016.162.00.00.00.008.AZ.TRO.HHZ.SAC")
 
-if !isfile(pmodel)
-    println("Downloading P-wave model... ",now())
-    url = "https://github.com/tclements/PhaseNet.jl/releases/download/Models/model_P.pt"
-    download(url,pmodel)
-    println("P-wave model complete... ",now())
+function get_release_data(file::String)
+    toget = basename(file)
+    url = "https://github.com/tclements/PhaseNet.jl/releases/download/Models/"
+    url = joinpath(url,toget)
+    println("Downloading $toget... ",now())
+    download(url,file)
+    println("$toget complete... ",now())
+    return nothing
 end
-if !isfile(smodel)
-    println("Downloading S-wave model... ",now())
-    url = "https://github.com/tclements/PhaseNet.jl/releases/download/Models/model_S.pt"
-    download(url,smodel)
-    println("S-wave model complete... ",now())
+
+for dat in [pmodel,smodel,edata,ndata,zdata]
+    if !isfile(dat)
+        get_release_data(dat)
+    end
 end
